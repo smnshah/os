3bf450cdb762e10e909b09fe9dd98cb62def24f9
Sameen Shah <smn.shah123@gmail.com>
2025-12-10 00:17:55 -0500

    Implement frame alloc for x86_64

---
+++ kernel/src/arch/x86_64/mem/mod.rs
@@ -0,0 +1,2 @@
+pub mod frame_alloc;
+pub mod memory_map;
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,19 +3,18 @@
 
 mod arch;
 
-use crate::arch::x86_64::{memory_map, serial};
+use crate::arch::x86_64::serial;
+use crate::arch::x86_64::mem::{frame_alloc, memory_map};
 use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)] 
 extern "C" fn kernel_main() -> ! {
-
     serial::init();
     serial::write_str(b"Hello world!\n");
 
-    memory_map::build_kernel_memory_map();
-    unsafe {
-        memory_map::HHDM_OFFSET = memory_map::get_hhdm_offset();
-    }
+    let regions = memory_map::build_kernel_memory_map();
+    let hhdm = memory_map::get_hhdm_offset();
+    frame_alloc::init(regions, hhdm);
 
     loop {}
 }
