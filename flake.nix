c9d821fcb9d36d0b4ef9dbd1cd5ffa06d56d992b
Sameen Shah <sameenshah@Sameens-MacBook-Pro.local>
2025-12-19 20:03:00 +0530

    Use nix and just to standardize builds.

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Rust OS development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, rust-overlay }:
+  let systems = [ 
+    "x86_64-linux" 
+    "aarch64-darwin" 
+  ];
+
+  forAllSystems = f:
+    nixpkgs.lib.genAttrs systems (system: f system);
+  
+  in
+  {
+    devShells = forAllSystems (system:
+      let pkgs = import nixpkgs { 
+	inherit system;
+	overlays = [ rust-overlay.overlays.default ];
+      };
+      in {
+        default = pkgs.mkShell {
+	  packages = with pkgs; [ 
+	    git
+	    just
+	    llvmPackages_latest.llvm
+	    llvmPackages_latest.lld
+	    qemu
+	    xorriso
+	    mtools
+	    autoconf
+	    automake
+	    nasm
+	    (rust-bin.nightly.latest.default.override {
+  	      targets = [ "x86_64-unknown-none" ];
+ 	    })
+	  ];
+
+	  shellHook = ''
+	    export PATH=${pkgs.git}/bin:$PATH
+	  '';
+        };
+      }
+    );
+  }; 
+}
+
--- kernel/src/arch/x86_64/limine_requests.rs
+++ kernel/src/arch/x86_64/limine_requests.rs
@@ -1,6 +1,9 @@
-use limine::request::{MemoryMapRequest, HhdmRequest};
+use limine::request::{HhdmRequest, MemoryMapRequest};
 
 #[used]
+#[unsafe(link_section = ".limine_reqs")]
 pub static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
 #[used]
+#[unsafe(link_section = ".limine_reqs")]
 pub static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+
