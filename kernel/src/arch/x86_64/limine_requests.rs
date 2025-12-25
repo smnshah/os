30d88d20eb6367458056954a88f3829b52c5f2f6
Sameen Shah <sameenshah@Sameens-MacBook-Pro.local>
2025-12-25 21:01:05 +0400

    Create limine base revision

--- kernel/src/arch/x86_64/limine_requests.rs
+++ kernel/src/arch/x86_64/limine_requests.rs
@@ -1,9 +1,14 @@
+use limine::BaseRevision;
 use limine::request::{HhdmRequest, MemoryMapRequest};
 
 #[used]
 #[unsafe(link_section = ".limine_reqs")]
+pub static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+#[used]
+#[unsafe(link_section = ".limine_reqs")]
 pub static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
+
 #[used]
 #[unsafe(link_section = ".limine_reqs")]
 pub static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
-
