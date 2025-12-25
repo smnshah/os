3a9cf4f089faeea37960bd6dc8e3f8b463bec9d0
Sameen Shah <smn.shah123@gmail.com>
2025-12-25 18:44:56 +0400

    Fix formatting

--- kernel/src/print.rs
+++ kernel/src/print.rs
@@ -1,7 +1,8 @@
 #[macro_export]
 macro_rules! print {
     ($fmt:expr $(, $arg:tt)*) => {{
-        use ::core::fmt::Write;            let mut serial = $crate::arch::x86_64::serial::Serial;
+        use ::core::fmt::Write;
+        let mut serial = $crate::arch::x86_64::serial::Serial;
         let _ = ::core::write!(serial, $fmt, $($arg)*);
     }};
 }
