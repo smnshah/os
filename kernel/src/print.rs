da7a12f2b7b938cab2d1e0db6201544728625004
Sameen Shah <sameenshah@Sameens-MacBook-Pro.local>
2025-12-25 18:43:06 +0400

    Fix indentation

--- kernel/src/print.rs
+++ kernel/src/print.rs
@@ -1,10 +1,9 @@
 #[macro_export]
 macro_rules! print {
-        ($fmt:expr $(, $arg:tt)*) => {{
-            use ::core::fmt::Write;
-            let mut serial = $crate::arch::x86_64::serial::Serial;
-            let _ = ::core::write!(serial, $fmt, $($arg)*);
-        }};
+    ($fmt:expr $(, $arg:tt)*) => {{
+        use ::core::fmt::Write;            let mut serial = $crate::arch::x86_64::serial::Serial;
+        let _ = ::core::write!(serial, $fmt, $($arg)*);
+    }};
 }
 
 #[macro_export]
