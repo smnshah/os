410649ada7f1692643218f669cbbf0bd0154e5be
Sameen Shah <smn.shah123@gmail.com>
2026-01-04 00:06:56 -0500

    Refactor translate into reusable helpers for map and unmap

--- kernel/src/mm/page.rs
+++ kernel/src/mm/page.rs
@@ -1,8 +1,7 @@
-use crate::arch::x86_64::mmu;
+use core::ptr::write_bytes;
 
-const HUGE_1G_MASK: u64 = 0x3FFF_FFFF;
-const HUGE_2M_MASK: u64 = 0x1F_FFFF;
-const PAGE_4K_MASK: u64 = 0xFFF;
+use crate::arch::x86_64::mmu;
+use crate::mm::frame;
 
 #[derive(Clone, Copy)]
 pub struct PageTableEntry {
@@ -11,8 +10,13 @@ pub struct PageTableEntry {
 
 impl PageTableEntry {
     const PRESENT: u64 = 1 << 0;
+    const WRITABLE: u64 = 1 << 1;
     const HUGE: u64 = 1 << 7;
     const ADDR_MASK: u64 = 0x000F_FFFF_FFFF_F000;
+    
+    pub fn new(value: u64) -> Self {
+        Self { entry: value }
+    }
 
     pub fn is_present(&self) -> bool {
         (self.entry & Self::PRESENT) != 0
@@ -33,40 +37,60 @@ pub struct PageTable {
 }
 
 impl PageTable {
-    fn get_entry(&self, virt_addr: u64, bit_shift: u32) -> PageTableEntry {
+    fn get_entry(&mut self, virt_addr: u64, bit_shift: u32) -> &mut PageTableEntry {
         let idx = ((virt_addr >> bit_shift) & 0x1FF) as usize;
-        self.entries[idx]
+        &mut self.entries[idx]
+    }
+
+    fn set_entry(&mut self, virt_addr: u64, bit_shift: u32, entry: PageTableEntry) {
+        let idx = ((virt_addr >> bit_shift) & 0x1FF) as usize;
+        self.entries[idx] = entry
     }
 }
 
-unsafe fn table_at(phys_addr: u64, hhdm_offset: u64) -> &'static PageTable {
-    unsafe { &*((phys_addr + hhdm_offset) as *const PageTable) }
+pub enum PteError {
+    HugePage,
+    NotMapped,
+    OutOfMemory,
 }
 
-pub fn translate(virt_addr: u64, hhdm_offset: u64) -> Option<u64> {
+pub enum MapError {
+    AlreadyMapped,
+    HugePage,
+    OutOfMemory,
+}
+
+pub fn map() {
+
+}
+
+pub fn unmap() {
+
+}
+
+unsafe fn table_at(phys_addr: u64, hhdm_offset: u64) -> &'static mut PageTable {
+    unsafe { &mut *((phys_addr + hhdm_offset) as *mut PageTable) }
+}
+
+fn get_pte_mut(virt_addr: u64, hhdm_offset: u64, allocate: bool) -> Result<&'static mut PageTableEntry, PteError> {
     let pml4 = unsafe { table_at(mmu::read_cr3() & PageTableEntry::ADDR_MASK, hhdm_offset) };
-    let pml4_entry = pml4.get_entry(virt_addr, 39);
-    if !pml4_entry.is_present() { return None; } 
+    
+    let pdp = get_or_allocate_table(pml4, virt_addr, 39, hhdm_offset, allocate)?;
+    if pdp.get_entry(virt_addr, 30).is_huge() { return Err(PteError::HugePage); } 
+    
+    let pd = get_or_allocate_table(pdp, virt_addr, 30, hhdm_offset, allocate)?;
+    if pd.get_entry(virt_addr, 21).is_huge() { return Err(PteError::HugePage); }
 
-    let pdp = unsafe { table_at(pml4_entry.addr(), hhdm_offset) };
-    let pdp_entry = pdp.get_entry(virt_addr, 30);
-    if !pdp_entry.is_present() { return None; }
+    let pt = get_or_allocate_table(pd, virt_addr, 21, hhdm_offset, allocate)?;
+    Ok(pt.get_entry(virt_addr, 12))
+}
 
-    if pdp_entry.is_huge() { 
-        return Some(pdp_entry.addr() | (virt_addr & HUGE_1G_MASK)); 
+fn get_or_allocate_table(page_table: &mut PageTable, virt_addr: u64, bit_shift: u32, hhdm_offset: u64, allocate: bool) -> Result<&'static mut PageTable, PteError> {
+    if !page_table.get_entry(virt_addr, bit_shift).is_present() { 
+        if !allocate { return Err(PteError::NotMapped); }
+        let new_table_phys = frame::alloc().ok_or(PteError::OutOfMemory)?;
+        unsafe { write_bytes((new_table_phys + hhdm_offset) as *mut u8, 0x00, 4096); }
+        page_table.set_entry(virt_addr, bit_shift, PageTableEntry::new(new_table_phys | PageTableEntry::PRESENT | PageTableEntry::WRITABLE));
     }
-
-    let pd = unsafe { table_at(pdp_entry.addr(), hhdm_offset) };
-    let pd_entry = pd.get_entry(virt_addr, 21);
-    if !pd_entry.is_present() { return None; }
-
-    if pd_entry.is_huge() {
-        return Some(pd_entry.addr() | (virt_addr & HUGE_2M_MASK));
-    }
-
-    let pt = unsafe { table_at(pd_entry.addr(), hhdm_offset) };
-    let pt_entry = pt.get_entry(virt_addr, 12);
-    if !pt_entry.is_present() { return None; }
-
-    Some(pt_entry.addr() | (virt_addr & PAGE_4K_MASK))
+    Ok(unsafe { table_at(page_table.get_entry(virt_addr, bit_shift).addr(), hhdm_offset) })
 }
