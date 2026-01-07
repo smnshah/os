4920d961e8b26c31c1f693e0a24d435848659f63
Sameen Shah <smn.shah123@gmail.com>
2026-01-06 22:34:33 -0500

    Fix print bug for multiple format string args

--- kernel/src/io/print.rs
+++ kernel/src/io/print.rs
@@ -3,17 +3,17 @@ macro_rules! print {
     ($fmt:expr $(, $arg:tt)*) => {{
         use ::core::fmt::Write;
         let mut serial = $crate::arch::x86_64::serial::Serial;
-        let _ = ::core::write!(serial, $fmt, $($arg)*);
+        let _ = ::core::write!(serial, $fmt $(, $arg)*);
     }};
 }
 
 #[macro_export]
 macro_rules! println {
-    ($fmt:expr $(, $arg:tt)*) => {
-        print!(concat!($fmt, "\n") $(, $arg)*);
-    };
+    ($fmt:expr $(, $arg:tt)*) => {{
+        print!(concat!($fmt, "\n") $(, $arg)*)
+    }};
 
-    () => {
-        print!("\n");
-    };
+    () => {{
+        print!("\n")
+    }};
 }
